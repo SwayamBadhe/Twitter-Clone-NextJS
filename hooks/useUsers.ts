5a42748a43cd485cae74e273b78dfa445bf09e69
SwayamBadhe <swayampbadhe@gmail.com>
2023-12-27 13:18:37 +0530

    Schema Changes

--- hooks/useUsers.ts
+++ hooks/useUsers.ts
@@ -2,13 +2,15 @@ import useSWR from 'swr';
 
 import fetcher from '@/libs/fetcher';
 
-const useUsers = (userId: string) => {
-  const { data, error, isLoading, mutate } = useSWR(
-    userId ? `/api/users/${userId}` : null,
-    fetcher
-  );
+const useUsers = () => {
+  const { data, error, isLoading, mutate } = useSWR('/api/users', fetcher);
 
-  return { data, error, isLoading, mutate };
+  return {
+    data,
+    error,
+    isLoading,
+    mutate,
+  };
 };
 
 export default useUsers;
